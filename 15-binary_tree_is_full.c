936d739425efb19c3c2a6471d2fd7d4ddbd1b924
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-06-04 17:59:01 +0300

    Fixed betty styling

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,20 +9,6 @@
  * Return: 1 if `tree` is full, 0 otherwise
  */
 
-/**
- * if tree == NULL -> return (0)
- * else
- *  	if (tree->right && tree->left)
- * 			func(tree->right)
- * 			func(tree->left)
- * 		else if (!tree->left && tree->right)
- * 			return (0)
- * 		else if (!tree->right && tree->left)
- * 			return (0)
- * 		else if (!tree->right && !tree->left)
- * 			return (1)
- */
-
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int check = 1;
