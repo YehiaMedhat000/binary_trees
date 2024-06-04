0674c97cc3801ac5e86133511525bf756968c9ed
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-06-04 14:43:16 +0300

    Added the 12th task

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,7 +4,15 @@
  * binary_tree_size - Measures the size of
  * a binary tree
  *
- * @tree: The node from which to measure
+ * @tree: The root node from which to measure
  *
  * Return: The size of the tree
  */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
