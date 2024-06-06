f1961265011e27128be1218277128a859dc68efe
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-06-06 07:56:53 +0300

    Fixed spaces to tabs issue

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -8,24 +8,24 @@
 
 void enqueue(queue_t *queue, binary_tree_t *node)
 {
-    queue_node_t *new_node = malloc(sizeof(queue_node_t));
+	queue_node_t *new_node = malloc(sizeof(queue_node_t));
 
-    if (new_node == NULL)
-        return;
+	if (new_node == NULL)
+		return;
 
-    new_node->node = node;
-    new_node->next = NULL;
+	new_node->node = node;
+	new_node->next = NULL;
 
-    if (queue->rear == NULL)
-    {
-        queue->front = new_node;
-        queue->rear = new_node;
-    }
-    else
-    {
-        queue->rear->next = new_node;
-        queue->rear = new_node;
-    }
+	if (queue->rear == NULL)
+	{
+		queue->front = new_node;
+		queue->rear = new_node;
+	}
+	else
+	{
+		queue->rear->next = new_node;
+		queue->rear = new_node;
+	}
 }
 
 /**
@@ -37,22 +37,22 @@ void enqueue(queue_t *queue, binary_tree_t *node)
 
 binary_tree_t *dequeue(queue_t *queue)
 {
-    queue_node_t *temp;
-    binary_tree_t *node;
+	queue_node_t *temp;
+	binary_tree_t *node;
 
-    if (queue->front == NULL)
-    {
-        return (NULL);
-    }
-    temp = queue->front;
-    node = temp->node;
-    queue->front = queue->front->next;
+	if (queue->front == NULL)
+	{
+		return (NULL);
+	}
+	temp = queue->front;
+	node = temp->node;
+	queue->front = queue->front->next;
 
-    if (queue->front == NULL)
-        queue->rear = NULL;
+	if (queue->front == NULL)
+		queue->rear = NULL;
 
-    free(temp);
-    return (node);
+	free(temp);
+	return (node);
 }
 
 /**
@@ -65,20 +65,21 @@ binary_tree_t *dequeue(queue_t *queue)
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    queue_t queue = {NULL, NULL};
-    if (tree == NULL || func == NULL)
-        return;
+	queue_t queue = {NULL, NULL};
 
-    enqueue(&queue, (binary_tree_t *)tree);
+	if (tree == NULL || func == NULL)
+		return;
 
-    while (queue.front != NULL)
-    {
-        binary_tree_t *current = dequeue(&queue);
-        func(current->n);
+	enqueue(&queue, (binary_tree_t *)tree);
 
-        if (current->left != NULL)
-            enqueue(&queue, current->left);
-        if (current->right != NULL)
-            enqueue(&queue, current->right);
-    }
+	while (queue.front != NULL)
+	{
+		binary_tree_t *current = dequeue(&queue);
+		func(current->n);
+
+		if (current->left != NULL)
+			enqueue(&queue, current->left);
+		if (current->right != NULL)
+			enqueue(&queue, current->right);
+	}
 }
